a7ebf52b140f8f5b687ae4e0cc43e6fcb43fd4fd
crfmc <crf85@cornell.edu>
2021-05-05 13:42:40 -0400

    adding good/bad comment

--- app/irsystem/models/result.py
+++ app/irsystem/models/result.py
@@ -14,7 +14,7 @@ class Result:
         - activity types (dict)
         - attributes (list)
         - description (string)
-        - reviews (string list)
+        - reviews (tuple list)
         - random review (string)
     """
 
@@ -27,7 +27,8 @@ class Result:
         self.difficulty = ith_trails['Difficulty']
         self.attributes = []
         # activities = ["Walking", "Hiking", "Running", "Biking", "Horseback Riding", "Cross-Country Skiiing", "Snowshoeing"]
-        activities = {"Walking": "static/walk-active.svg", "Hiking":"static/hike-active.svg", "Running":"static/run-active.svg", "Biking":"static/bike-active.svg", "Horseback Riding":"static/horse-active.svg", "Cross-Country Skiiing":"static/ski-active.svg", "Snowshoeing":"static/snowshoe-active.svg"}
+        activities = {"Walking": "static/walk-active.svg", "Hiking": "static/hike-active.svg", "Running": "static/run-active.svg", "Biking": "static/bike-active.svg",
+                      "Horseback Riding": "static/horse-active.svg", "Cross-Country Skiiing": "static/ski-active.svg", "Snowshoeing": "static/snowshoe-active.svg"}
         # self.activity_types = { i : False for i in activities }
         self.activity_types = []
         for attribute in ith_trails['Trail Attributes']:
@@ -39,16 +40,38 @@ class Result:
         self.description = ith_trails['Description']
         self.reviews = []
         for review in ith_trails["Reviews"]:
-            if review['comment'] != "":
-                self.reviews.append(review["comment"])
-        self.review = self.reviews[random.randint(0, len(self.reviews)-1)]
+            if review['comment'] != "" and review['rating'] != None:
+                self.reviews.append((review["comment"], review["rating"]))
+                # self.reviews.append(review)
+        # self.review = self.reviews[random.randint(0, len(self.reviews)-1)]
+        self.review = self.getReviews(self.reviews)
         self.img = ith_trails['image id']
         print(self.img)
-        self.url = "https://ithacatrails.org/trail/" + str(ith_trails["Ithacatrails ID"])
+        self.url = "https://ithacatrails.org/trail/" + \
+            str(ith_trails["Ithacatrails ID"])
         # self.accessibility_types = kwargs.get('accessibility_types')
 
-        
-        
+    def getReviews(self, reviews):
+        """
+        Returns a dicitonary: 
+            { 'good' : [good_review], 'bad' : [bad_review] }
+
+        where [good_review] is a review with a positive sentiment, and a 
+        [bad_review] is a review with a negative sentiment.
+
+        A good review should have a high
+        """
+        # print(reviews)
+
+        sorted_reviews = sorted(
+            reviews, key=lambda tup: (tup[1], -len(tup[0])))
+        return {
+            'good': sorted_reviews[len(sorted_reviews) - 1],
+            'bad': sorted_reviews[0]
+        }
+        # return sorted_reviews
+
+
 # Test Code
 # ---------
 # rslt = Result((.25, "Ellis Hollow Yellow trail"))
@@ -61,6 +84,3 @@ class Result:
 # print(rslt.reviews)
 # print(rslt.attributes)
 # print(rslt.activity_types)
-
-
-
